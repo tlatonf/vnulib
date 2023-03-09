4d8d49f2911bf44251c96062addb7f8454e223cc
Toan Huynh <toanhp242@gmail.com>
2023-03-09 16:41:27 +0700

    update 16:36 09/03/2023
    đã:
    - tiếp tục đưa các chức năng riêng lẻ vào các funtion
    - tạo menu chọn range page
    
    sẽ:
    - check link nhập vào
    - làm lại ascii art (tạo màn hình kết thúc)
    - đặt lại tên các biến, func, comment

--- downloader_v1.2.bat
+++ downloader_v1.2.bat
@@ -1,68 +1,83 @@
 :: https://ir.vnulib.edu.vn/flowpaper/simple_document.php?subfolder=23/91/22/&doc=23912253043895351368360641452395615385&bitsid=f96fb9d3-833e-4798-819f-1fe46243be67&uid=f5c8452e-f096-4ed8-bf5c-2a92357b9db7
 
-rem config batch
-@echo off
-title VNULIB DOWNLOADER BY TLATONF
+:run
+
+:: BEGIN CONFIG_BATCH
 mode con:cols=80 lines=30
+@echo off
 color 0A
+title VNULIB DOWNLOADER BY TLATONF
+:: END CONFIG_BATCH
 
-rem global var
-set subfolder=NONE
-set doc=NONE
-set state=NONE
+:: BEGIN DECLARE_GLOBAL_VARIABLE
+set subfolder=
+set doc=
+set state=
+set from_page=
+set to_page=
+:: END DECLARE_GLOBAL_VARIABLE
+
+        :: BEGIN MAIN
+        setlocal EnableDelayedExpansion
+
+        :::: BEGIN INPUT_LINK_EBOOK
+        call :HEADER_PRINTER
+        set /p link=LINK: 
+        set link=%link:&=*%
+        call :FIND "%link%"
+        :::: END INPUT_LINK_EBOOK
+
+        set adr=VNULIB_Downloader
+        mkdir "%~dp0%adr%"
+        rem call :CREATE_LOG %~dp0%adr% %link%
+
+        call :MENU
+        :start_program
+        set adr=VNULIB_Downloader\image
+        mkdir "%~dp0%adr%"
+        rem explorer %adr%
+        rem start_program /min "" "%SystemRoot%\explorer.exe" /n, /e, "%cd%\%adr%"    
+
+        :::: BEGIN DOWNLOAD_LOOP
+        set i=%from_page%
+        :do_while
+            call :HEADER_PRINTER
+            echo DOWNLOADING PAGE %i%...
+
+            set url=https://ir.vnulib.edu.vn/flowpaper/services/view.php?doc=%doc%^&format=jpg^&page=!i!^&subfolder=%subfolder%
+            set adr=VNULIB_Downloader\image\page_!i!.jpg
+            curl -o !adr! "!url!"
+
+            call :CHECK_FILE_AVAILABEL !adr!
+            if !state! EQU FILE_FAIL goto done
+            
+            if !i! EQU %to_page% goto done
+            set /a i+=1
+        goto do_while
+        :::: END DOWNLOAD_LOOP
+
+        :done
+
+        rem debuger
+        pause
+        goto run
+        
+        pause
+        exit
+
+        rem endlocal
+        :: END MAIN
 
 
-:: begin script
-setlocal EnableDelayedExpansion
+:: BEGIN FUNCTION
 
-rem input link ebook
-call :header
-set /p link=LINK: 
-set link=%link:&=*%
-
-set adr=VNULIB_Downloader
-mkdir "%~dp0%adr%"
-call :create_log %~dp0%adr% %link%
-call :find "%link%"
-
-echo.
-set /p from_page=FROM PAGE: 
-echo.
-set /p to_page=TO PAGE: 
-
-set adr=VNULIB_Downloader\image
-mkdir "%~dp0%adr%"
-rem explorer %adr%
-rem start /min "" "%SystemRoot%\explorer.exe" /n, /e, "%cd%\%adr%"
-
-rem download image from vnulib
-set i=%from_page%
-:download
-call :header
-echo DOWNLOADING PAGE %i%...
-set url=https://ir.vnulib.edu.vn/flowpaper/services/view.php?doc=%doc%^&format=jpg^&page=!i!^&subfolder=%subfolder%
-set adr=VNULIB_Downloader\image\page_!i!.jpg
-curl -o !adr! "!url!"
-call :check_file_available !adr!
-if %state%==FILE_FAIL goto done
-set /a i+=1
-if !i! LEQ %to_page% goto download
-
-:done
-pause
-
-endlocal
-:: end script
-
-
-rem this is space for functions
-
-:create_log
-    echo [InternetShortcut] >> "%1\LINK.url"
-    echo URL=%2 >> "%1\LINK.url"
+:CREATE_LOG
+    :: STATEMANT_HERE
+    rem echo [InternetShortcut] >> "%1\LINK.url"
+    rem echo URL=%2 >> "%1\LINK.url"
 goto :eof
 
-:find
+:FIND
     for /f "tokens=1,2 delims=*" %%a in (%1) do (
         for /f "tokens=2 delims==" %%c in ("%%a") do (
             set "subfolder=%%c"
@@ -73,15 +88,81 @@ goto :eof
     )
 goto :eof
 
-:check_file_available
-    for /f "tokens=3" %%a in ('dir /-c %1 ^| findstr /i "file(s)"') do set size=%%a
+:CHECK_FILE_AVAILABEL
+    for /f "tokens=3" %%a in ('dir /-c %1 ^| FINDstr /i "file(s)"') do set size=%%a
     if %size% LEQ 1000 (
         del %1
         set state=FILE_FAIL
     )
 goto :eof
 
-:header
+:MENU
+    rem call :HEADER_PRINTER
+    call :MENU_PRINTER
+    :input_choice
+    
+    set /p choice=--^>      ENTER YOUR CHOICE: 
+    if %choice%==A goto option1
+    if %choice%==a goto option1
+    if %choice%==S goto option2
+    if %choice%==s goto option2
+    if %choice%==B goto option3
+    if %choice%==b goto option3
+    if %choice%==E goto done
+    if %choice%==e goto done
+    echo INVALID CHOICE. PLEASE TRY AGAIN....
+    goto input_choice
+
+        :option1
+        set from_page=1
+        set to_page=0
+        goto start_program
+        
+        :option2
+        call :MENU_PRINTER
+        echo --^>      ENTER SELECT RANGE PAGE
+        :re_input_range_page
+        set /p from_page=--^>      FROM PAGE: 
+        set /p to_page=--^>      TO PAGE: 
+        
+        :: BEGIN CHECK_INPUT_RANGE
+        if 0 GEQ %from_page% (
+            echo CONDITION: 0 ^< FROM PAGE ^<= TO PAGE
+            goto re_input_range_page
+        )
+        if %from_page% GTR %to_page% (
+            echo CONDITION: 0 ^< FROM PAGE ^<= TO PAGE
+            goto re_input_range_page
+        )
+        :: END CHECK_INPUT_RANGE
+
+        goto start_program
+
+        :option3
+        goto run
+
+goto :eof
+
+:MENU_PRINTER
+    cls
+    echo.
+    echo             ____    ____     ________     ____  _____     _____  _____  
+    echo           ^|_   \  /   _^|   ^|_   __  ^|   ^|_   \^|_   _^|   ^|_   _^|^|_   _^| 
+    echo             ^|   \/   ^|       ^| ^|_ \_^|     ^|   \ ^| ^|       ^| ^|    ^| ^|   
+    echo             ^| ^|\  /^| ^|       ^|  _^| _      ^| ^|\ \^| ^|       ^| ^|    ^| ^| 
+    echo            _^| ^|_\/_^| ^|_     _^| ^|__/ ^|    _^| ^|_\   ^|_      ^| ^|____^| ^|
+    echo           ^|_____^|^|_____^|   ^|________^|   ^|_____^|\____^|     ^|________^|                                                             
+    echo.
+    echo                            CHOICE DOWNLOAD RANGE PAGE                           
+    echo.
+    echo          [A]. ALL RANGE PAGE                                                    
+    echo          [S]. SELECT RANGE PAGE                                                 
+    echo          [B]. BACK TO ENTER LINK                                                
+    echo          [E]. EXIT                                                              
+    echo.                                                                                
+goto :eof
+
+:HEADER_PRINTER
     cls
     echo ********************************************************************************
     echo ****************** SCRIPT DOWNLOAD EBOOK FROM VNUHCM LIBRARY *******************
@@ -103,3 +184,5 @@ goto :eof
     echo ********************************************************************************
     echo.
 goto :eof
+
+:: BEGIN FUNCTION
